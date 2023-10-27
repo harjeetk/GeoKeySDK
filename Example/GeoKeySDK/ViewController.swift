65b75af16e96ceb9c1350ee723c44c3ae69befe4
Harjeet <harjeetsinghkapule03@gmail.com>
2023-10-27 10:44:46 +0530

    Initial commit

---
+++ Example/GeoKeySDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GeoKeySDK
+//
+//  Created by Harjeet on 10/27/2023.
+//  Copyright (c) 2023 Harjeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
